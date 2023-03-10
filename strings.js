9e197892e51772708d666097373cd28befffd473
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-03-10 20:13:20 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,16 @@
+// Extract the first four characters from the string below;
+// "beautiful"
+let description="beautiful";
+console.log(description[0]);
+console.log(description[1]);
+console.log(description[2]);
+console.log(description[3]);
+
+// Insert the following string at the tenth index of the below variable:
+// "eat"
+// const food = "I did not have appetite today"
+const food ="I did not have appetite today"
+let x="eat"
+console.log(food.replace(food[10] ,"eat"))
+
+
